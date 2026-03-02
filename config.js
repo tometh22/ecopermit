e91aa1793e6babf32724241ae31f901db460b30e
tometh22 <114619926+tometh22@users.noreply.github.com>
2026-03-02 14:00:07 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.APP_CONFIG = {
-  GMAPS_API_KEY: "",
-  API_BASE_URL: "",
+  GMAPS_API_KEY: "AIzaSyA6sdbBuvD6MlTz10F-V0MkAaXMSMn8IrM",
+  API_BASE_URL: "https://ecopermit-backend.onrender.com",
 };
